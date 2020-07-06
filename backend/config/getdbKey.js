68c5df82afe82df73f63f925f63a3c9c2c7e6175
Senbonzakura <anhdungpham090@gmail.com>
2020-07-07 00:32:22 +0700

    use .env instead of key.js file.

--- backend/config/getdbKey.js
+++ backend/config/getdbKey.js
@@ -1,10 +1,10 @@
-const keys = require('./keys');
+require('dotenv').config();
 decode64 = function(key){
     let buff = new Buffer.from(key, 'base64');
     return buff.toString('ascii');
 }
 module.exports = {
-    adminURI: decode64(keys.admin),
-    moderatorURI: decode64(keys.moderator),
-    userURI: decode64(keys.user)
+    adminURI: decode64(process.env.DB_URI_ADMIN),
+    moderatorURI: decode64(process.env.DB_URI_MODERATOR),
+    userURI: decode64(process.env.DB_URI_USER)
 }
